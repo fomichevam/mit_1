046d6a5ce7ee929024e50fddc52be15e84b89293
mfomicheva <msfom04@mail.ru>
2025-06-06 22:22:00 +0400

    feat: add fibonacci starting with 1

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+
+int fib(int n){
+	if (n <= 2){
+		return 1;
+	}
+	return fib(n - 1) + fib(n - 2);
+}
+
 int main(){
-std::cout << "Hello world";
-retur 0;
+	std::cout << "Hello world";
+	std::cout << "\n";
+	std::cout << fib(10);
+	return 0;
 }
